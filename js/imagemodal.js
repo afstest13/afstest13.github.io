4edf993a92ae25ef71899e1d13054a7b449f25de
sakshi <sakshijain908@gmail.com>
2018-03-19 13:39:54 +0530

    halfblood

--- js/imagemodal.js
+++ js/imagemodal.js
@@ -2,17 +2,21 @@
 var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
+var imgArray = document.getElementsByClassName("pop");
 var modalImg = document.getElementById("img01");
 
 var captionText = document.getElementById("caption");
 
-img.onclick = function(){
+var clickFunction = function(){
 	var sticky_wrapper=document.getElementById("sticky-wrapper");
 	sticky_wrapper.style.display="none";
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
+}
+for(var i=0;i< imgArray.length ;i++)
+{
+	imgArray[i].addEventListener('click',clickFunction,false);
 }
 
 // Get the <span> element that closes the modal
